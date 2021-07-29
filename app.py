4a6e9e5837f952e47879477b8c2b63cc66fd1a5c
eschilb <eschilb@gmail.com>
2021-07-29 04:34:45 +0000

    Add course materials

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask,jsonify
+from flask import Flask
+from flask import json
 import logging
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
 @app.route('/status')
-def get_status():
-    logging.info('{{TIMESTAMP}}, {{ ENDPOINT_NAME}} endpoint was reached')
-    return jsonify(result="OK - healthy")
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Status request successfull')
+    return response
 
 @app.route('/metrics')
-def get_metrics():
-    logging.info('{{TIMESTAMP}}, {{ ENDPOINT_NAME}} endpoint was reached')
-    return jsonify(data={'UserCount': 140, 'UserCountActive': 23})
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request successfull')
+
+    return "Hello World!"
 
 if __name__ == "__main__":
+    ## stream logs to a file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
